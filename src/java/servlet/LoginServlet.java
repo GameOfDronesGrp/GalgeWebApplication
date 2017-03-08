b58d600d808b5f0f4f0c0da557db2c180b870bec
HaydarTas <haydartas1982@gmail.com>
2017-03-08 13:25:26 +0100

    Login virker

--- src/java/servlet/LoginServlet.java
+++ src/java/servlet/LoginServlet.java
@@ -7,11 +7,16 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
 
 /**
  *
@@ -20,32 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
+   
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -58,31 +38,24 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       
+        URL url = new URL("http://130.226.195.227:9591/galgelegtjeneste?wsdl");
+        QName qname = new QName("http://galgeleg/", "GalgelogikService");
+        Service service = Service.create(url, qname);
+        
+        
+        response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+        
+        String n=request.getParameter("username");  
+        String p=request.getParameter("password"); 
+        
+        HttpSession session = request.getSession(false);
+        if(session!=null)
+        session.setAttribute("name", n);
+
+          
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
--- web/Index.jsp
+++ web/Index.jsp
@@ -1,7 +1,11 @@
 
 
+<%@page import="javax.xml.namespace.QName"%>
+<%@page import="javax.xml.ws.Service"%>
+<%@page import="java.net.URL"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
+
 <html>
     <head>
         <title>Galgespil</title>
@@ -11,9 +15,9 @@
     
     <body>
         <h1 style="margin-top: 30px;"><center>Velkommen til Galgelegspillet</center></h1>
-<p style="margin-top: 30px;"><center>Login for at start spilet</center></p>
+    <p style="margin-top: 30px;"><center>Login for at start spilet</center></p>
 
-<form>
+    <form>
     <center>        
         <label for="username">User Name:</label>
         <br>
@@ -26,11 +30,34 @@
         <br>
         <input type="submit" value="Login"> 
     </center>
+    </form>
+
+
+
     
+    <%
+    try {
+	galgeleg.GalgelogikService service = new galgeleg.GalgelogikService();
+	galgeleg.GalgelegI port = service.getGalgelogikPort();
+	 // TODO initialize WS operation arguments here
+	java.lang.String arg0 = request.getParameter("username");
+	java.lang.String arg1 = request.getParameter("password");
+	// TODO process result here
+	boolean result = port.hentBruger(arg0, arg1);
+	out.println("Result = "+result);
+            if(result){
+            response.sendRedirect("Spil.jsp");
+        }else {
+            response.sendRedirect("Index.jsp");
+            out.println("Forkert brugernavn eller adgangskode");
+        }
+    } catch (Exception ex) {
+	// TODO handle custom exceptions here
+    }
+    
+
+
+    %>
+
+    </body> 
 </html>
-
-<%
-
-
-
-%>
